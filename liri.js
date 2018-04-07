80faa7e913fcf5d4d364f6ec3ea80e4eaa348c53
Ryan Mullowney <Batman@Ryans-MacBook-Pro.local>
2018-04-06 23:04:51 -0500

    Adds Spotify functionality

--- liri.js
+++ liri.js
@@ -1,19 +1,23 @@
 (function () {
+    require("dotenv").config();
     var Twitter = require('twitter');
     var Spotify = require('node-spotify-api');
     var request = require('request-promise');
-    require("dotenv").config();
 
     // Import the keys.js file
     let keys = require('./keys.js')
+    debugger;
 
     // Assign the keys.js content to variables
-    var spotify = new Spotify(keys.spotify);
+    var spotify = new Spotify({
+        id: keys.spotify.id,
+        secret: keys.spotify.secret
+    });
     var client = new Twitter(keys.twitter);
 
-    // Both are undefined right now - why?
-    console.log(spotify.id)
-    console.log(client.consumer_key)
+    // console.log(spotify)
+    // console.log(spotify.credentials.id)
+    // console.log(client.consumer_key)
 
     // Take the first word of the player input to determine the action
     let action = process.argv[2];
@@ -29,10 +33,35 @@
     // Formats the user input
     userInput = userInputArray.join(" ")
 
+    // If the user doesn't provide a specific song, it defaults to The Sign by Ace of Base
+    if (action === "spotify-this-song" && userInputArray.length == 0){
+            userInput = "The Sign Ace of Base"
+        
+    }
+
 
     switch (action) {
         case "my-tweets":
+
         case "spotify-this-song":
+            spotify.search({
+                type: 'track',
+                query: userInput,
+                limit: 1,
+            }, function (err, data) {
+                if (err) {
+                    return console.log('Error occurred: ' + err);
+                }
+                console.log(`
+                Artist(s): ${data.tracks.items[0].artists[0].name}
+                Song title: ${data.tracks.items[0].name}
+                Album: ${data.tracks.items[0].album.name}
+                Preview link: ${data.tracks.items[0].preview_url}
+                `)
+                
+            })
+            break;
+
         case "movie-this":
             request("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy") // What if plot isn't short, and what is y=?
                 .then(response => {
@@ -48,8 +77,9 @@
         Actors: ${data.Actors}
         `)
                 })
+                break;
+
         case "do-what-it-says":
     }
-
 
 })();
